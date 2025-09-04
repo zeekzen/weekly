d72f2588766c0624144c86a165d238271c2b231a
yangzitao <yangzitao.1995@bytedance.com>
2025-09-04 10:56:10 +0800

    test

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 export const SITE = {
-  title: "潮流周刊",
-  author: "Tw93",
-  description: "记录工程师 Tw93 的不枯燥生活，每周一发布，欢迎关注",
+  title: "Daily测试",
+  author: "Zeekzen",
+  description: "记录工程师 Zeekzen 的枯燥生活，每天发布，欢迎关注",
   keywords: "Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pake,Swift,设计",
   icon: "https://gw.alicdn.com/imgextra/i2/O1CN01m9YYjS1QBeW5DOm3I_!!6000000001938-2-tps-400-400.png",
   siteImage:
     "https://gw.alicdn.com/imgextra/i4/O1CN010j3h201UDilmn716N_!!6000000002484-2-tps-1280-640.png",
-  homePage: "https://weekly.tw93.fun",
-  blogPage: "https://tw93.fun/",
-  twitterId: "HiTw93",
-  githubId: "tw93",
-  repo: "tw93/weekly",
+  homePage: "https://weekly-five-nu.vercel.app",
+  blogPage: "https://weekly-five-nu.vercel.app",
+  twitterId: "",
+  githubId: "zeekzen",
+  repo: "zeekzen/weekly",
 };
